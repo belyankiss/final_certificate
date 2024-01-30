099609729ade14de0282e42f5a46a1965f817670
belyankiss <belyankiss@gmail.com>
2024-01-30 20:05:38 +0300

    Создал программу для разворачивания массива при помощи рекурсии

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] array = {12, 34, 28, 5, 6, 0, 12, 43};
+InverseArray(array, array.Length-1);
+
+static void InverseArray(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        InverseArray(array, index-1);
+    }
+}
