1a6d726a60cea5e8e54784f7ede24d069b40eb7a
belyankiss <belyankiss@gmail.com>
2024-01-30 20:04:58 +0300

    Создал программу для выполнения функции Аккермана

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = 3, n = 2;
+Console.WriteLine($"Значение функции Аккермана для m = {m} и n = {n} : {Ackermann(m, n)}");
+
+static int Ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+
+    if (m > 0 && n == 0)
+        return Ackermann(m-1, 1);
+
+    if (m > 0 && n > 0)
+        return Ackermann(m-1, Ackermann(m, n-1));
+
+    return -1;
+}
